aa7f2e6c7af2502257cf5466b6eabc6c72b93c84
Amber Connelly <113668892+ac-z@users.noreply.github.com>
2024-03-31 18:08:02 -0700

    Add .gitignore and remove hash feature (it's slow, and btrfs hashes anyway)

--- start-backup.sh
+++ start-backup.sh
@@ -17,30 +17,14 @@ tmbackup="./rsync-time-backup/rsync_tmbackup.sh"
 # Exit if the backup script doesn't exist
 if [ ! -f "$tmbackup" ]; then
     echo "Backup script not found. "
-    exit 0
-fi
-
-# Set the name of the safety hash file
-hash_file="${script_dir}/tm/_hashes"
-# If the hash file exists, check it
-if [ -f "$hash_file" ]; then
-    sha256sum -c "$hash_file" --quiet
-    if [ $? -ne 0 ]; then
-        echo "The backup hash doesn't match. Currently backed-up files could be corrupted. Cannot continue."
-        echo "Exiting..."
-        exit 0
-    else
-        echo "Hash matches. Continuing..."
-    fi
-else
-    echo "No hash file found. Continuing..."
+    exit 1
 fi
 
 # And the logs directory
-log_dir="$script_dir/tm/_logs"
+log_dir="$script_dir/backups/_logs"
 mkdir -p "$log_dir"
 # And the backup directory
-backup_dir="$script_dir/tm/$backup_name"
+backup_dir="$script_dir/backups/$backup_name"
 mkdir -p "$backup_dir"
 touch "$backup_dir/backup.marker"
 
@@ -48,7 +32,7 @@ touch "$backup_dir/backup.marker"
 exclude_file="$script_dir/exclude.txt"
 if [ ! -f "$exclude_file" ]; then
     echo "No exclude.txt found. Exiting..."
-    exit 0
+    exit 1
 fi
 
 # Start the backup
@@ -58,16 +42,9 @@ $tmbackup --log-dir "${log_dir}/" "${backup_target}/" "${backup_dir}/" $exclude_
 # Ensure the backup succeeded
 if [ $? -ne 0 ]; then
     echo "Backup failed. Exiting..."
-    exit 0
-fi
-
-# Create $hash_file file listing the hashes of every file in $script_dir/tm
-echo "==> Creating hash file..."
-find "$script_dir/tm" -type f -exec sha256sum {} \; > "$hash_file"
-
-# Ensure the hash succeeded
-if [ $? -ne 0 ]; then
-    echo "Hashing failed. Exiting..."
+    exit 1
+else
+    echo "Backup succeeded. Exiting..."
     exit 0
 fi
 
